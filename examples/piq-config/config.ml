b9f590bed7e62fd38c2adec89bf881a8be9f8a03
Anton Lavrik <alavrik@piqi.org>
2014-03-16 03:32:48 -0500

    Update piq-config example to use frameless piq

--- examples/piq-config/config.ml
+++ examples/piq-config/config.ml
@@ -8,10 +8,16 @@ let read_file filename =
   Buffer.contents buf
 
 
+(* common serialization options *)
+let opts = Piqirun_ext.make_options ()
+  ~piq_frameless_input:true
+  ~piq_frameless_output:true
+
+
 let read_config filename =
   let contents = read_file filename in
   try
-    Config_piqi_ext.parse_config contents `piq
+    Config_piqi_ext.parse_config contents `piq ~opts
   with
     Piqi_common.Error ((file, col, line), error) ->
       failwith (Printf.sprintf "error at %s:%d:%d: %s" file col line error)
@@ -20,5 +26,5 @@ let read_config filename =
 (* test *)
 let _ =
   let config = read_config "config.piq" in
-  Config_piqi_ext.print_config config
+  Config_piqi_ext.print_config config ~opts
 
