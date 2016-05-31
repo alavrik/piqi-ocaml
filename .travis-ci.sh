735159402c903b6358deb7b518eb57b19ea04027
Anton Lavrik <alavrik@piqi.org>
2016-05-31 03:00:49 -0700

    Update opam file and travis-ci scripts

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -8,20 +8,10 @@ sudo apt-get install protobuf-compiler
 
 
 # build, run tests, build package, install package
-wget https://raw.githubusercontent.com/ocaml/ocaml-travisci-skeleton/master/.travis-opam.sh
+wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-opam.sh
+
+
+# opam lint is way too demanding and varies from version to version
+export OPAM_LINT=false
 
 . .travis-opam.sh
-
-
-# build and test using the current development version of piqilib
-if [ -n "${PIQILIB_DEV-}" ]
-then
-    opam repo add piqi git://github.com/piqi/piqi-opam-repo.git
-
-    opam install piqilib.master
-
-    make distclean
-    make
-    make test
-fi
-
