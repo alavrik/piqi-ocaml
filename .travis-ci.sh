5702a2c5eba5c327bcb04ad0db88d564312df415
Anton Lavrik <alavrik@piqi.org>
2016-06-01 22:52:50 -0700

    Update opam file

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -11,7 +11,4 @@ sudo apt-get install protobuf-compiler
 wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-opam.sh
 
 
-# opam lint is way too demanding and varies from version to version
-export OPAM_LINT=false
-
 . .travis-opam.sh
