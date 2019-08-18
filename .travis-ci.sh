7a9374276ea3930148a407e3e50e66b254237e23
Anton Lavrik <alavrik@piqi.org>
2019-08-18 22:51:32 +0100

    travis-ci: try to fix build

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,11 +2,6 @@
 
 set -ex
 
-
-# optional dependencies for running tests
-sudo apt-get install protobuf
-
-
 # build, run tests, build package, install package
 wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-opam.sh
 
